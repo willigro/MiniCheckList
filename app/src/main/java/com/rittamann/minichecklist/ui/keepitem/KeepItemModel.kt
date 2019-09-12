152270ccc5618fb075cd7c1c9e63c634da613e05
willirittmann <willi.oliveira@trinitysolucoes.com>
2019-09-12 16:51:31 -0300

    KeepItemViewModel base

--- app/src/main/java/com/rittamann/minichecklist/ui/keepitem/KeepItemModel.kt
+++ app/src/main/java/com/rittamann/minichecklist/ui/keepitem/KeepItemModel.kt
@@ -1,8 +1,12 @@
 package com.rittamann.minichecklist.ui.keepitem
 
 import android.content.Context
+import com.rittamann.minichecklist.data.base.Item
 import com.rittamann.minichecklist.data.repository.CheckListDAO
 
 class KeepItemModel(context: Context) {
     private val checkListDAO = CheckListDAO(context)
+
+    fun insert(item: Item) = checkListDAO.insert(item)
+    fun update(item: Item) = checkListDAO.update(item)
 }
--- app/src/main/java/com/rittamann/minichecklist/ui/keepitem/KeepItemViewModel.kt
+++ app/src/main/java/com/rittamann/minichecklist/ui/keepitem/KeepItemViewModel.kt
@@ -6,10 +6,31 @@ import com.rittamann.minichecklist.ui.base.BaseViewModel
 
 class KeepItemViewModel(private val keepItemModel: KeepItemModel) : BaseViewModel() {
 
-    private val keptItemResult: MutableLiveData<Item> = MutableLiveData()
-    fun getKeptItem() = keptItemResult
+    private val insertResult: MutableLiveData<Item> = MutableLiveData()
+    private val updateResult: MutableLiveData<Item> = MutableLiveData()
+    private val contentValidateResult: MutableLiveData<Boolean> = MutableLiveData()
+    fun getKeptItem() = insertResult
+    fun getContentResult() = contentValidateResult
 
     fun keep(item: Item) {
-        
+        contentValidateResult.value = true
+        if (item.validName().not()) {
+            contentValidateResult.value = false
+        } else {
+            if (item.id == 0L) insert(item) else update(item)
+        }
+    }
+
+    private fun insert(item: Item) {
+        keepItemModel.insert(item).also {
+            item.id = it
+            insertResult.value = if (it > 0) item else null
+        }
+    }
+
+    private fun update(item: Item) {
+        keepItemModel.update(item).also {
+            updateResult.value = if (it) item else null
+        }
     }
 }
