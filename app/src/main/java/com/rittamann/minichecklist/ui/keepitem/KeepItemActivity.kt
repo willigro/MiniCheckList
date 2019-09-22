f4d72488d028b677cbf4f15305d40cfbe5c1514d
willirittmann <willi.oliveira@trinitysolucoes.com>
2019-09-22 10:05:56 -0300

    Implementado selecao e timer

--- app/src/main/java/com/rittamann/minichecklist/ui/keepitem/KeepItemActivity.kt
+++ app/src/main/java/com/rittamann/minichecklist/ui/keepitem/KeepItemActivity.kt
@@ -9,10 +9,13 @@ import com.rittamann.minichecklist.data.base.Item
 import com.rittamann.minichecklist.ui.base.BaseActivity
 import com.rittamann.minichecklist.utils.Constants
 import kotlinx.android.synthetic.main.activity_keep_item.editTextContent
+import java.util.Timer
+import kotlin.concurrent.schedule
 
 class KeepItemActivity : BaseActivity() {
 
     private lateinit var viewModel: KeepItemViewModel
+    private var timer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,15 +35,37 @@ class KeepItemActivity : BaseActivity() {
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                viewModel.update(p0.toString())
+                if (timer == null) {
+                    initTimer()
+                } else {
+                    timer!!.cancel()
+                    initTimer()
+                }
+
+                timer!!.schedule(TIME_TO_UPDATE) {
+                    runOnUiThread {
+                        viewModel.update(p0.toString())
+                    }
+                }
             }
         })
+    }
+
+    private fun initTimer() {
+        timer = Timer("", false)
     }
 
     private fun initObserver() {
         viewModel.getItemAttached()
             .observe(this, Observer { item ->
-                item?.also { editTextContent.setText(item.content) }
+                item?.also {
+                    editTextContent.setText(it.content)
+                    editTextContent.setSelection(it.content.length)
+                }
             })
+    }
+
+    companion object {
+        const val TIME_TO_UPDATE: Long = 500
     }
 }
